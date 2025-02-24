f94fbbbf263966e70ecb3abfa8fb389a3c6b2833
Tong Ki Chun <kctong529@gmail.com>
2025-02-24 22:28:00 +0200

    Fix the path

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   testEnvironment: 'jest-environment-jsdom',  // Required for browser-like environment
-  roots: ['<rootDir>/__test__'], // Point to the tests folder
+  roots: ['<rootDir>/__tests__'], // Point to the tests folder
   transform: {
     '^.+\\.js$': 'babel-jest'  // Use Babel to handle ES Modules
   }
