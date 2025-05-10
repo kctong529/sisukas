8f9f64cdcaec6f7a5b464b3e155e7ba6b99b75c4
Tong Ki Chun <kichun.tong@aalto.fi>
2025-05-10 10:53:27 +0300

    Update subject to include issue number

--- scripts/send_newsletter.sh
+++ scripts/send_newsletter.sh
@@ -6,6 +6,12 @@ html_content=$(<public/newsletter.html)
 # Read emails from the file and format them as a JSON array
 email_list=$(jq -Rn '[inputs]' < email_list.txt)
 
+# Get current issue number
+issue_number=$(date +'%Y Week %V')
+
+# Construct subject line
+subject="Sisukas Newsletter - $issue_number"
+
 # Send the email using Resend API with jq for proper escaping
 curl -X POST 'https://api.resend.com/emails' \
   -H "Authorization: Bearer $RESEND_API_KEY" \
@@ -14,6 +20,6 @@ curl -X POST 'https://api.resend.com/emails' \
     --arg from "newsletter@sisukas.eu" \
     --arg to "Sisukas Subscribers <kichun.tong@aalto.fi>" \
     --argjson bcc "$email_list" \
-    --arg subject "Sisukas Newsletter - Upcoming Courses" \
+    --arg subject "$subject" \
     --arg html_content "$html_content" \
     '{from: $from, to: $to, bcc: $bcc, subject: $subject, html: $html_content}')"
